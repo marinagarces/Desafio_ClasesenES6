5c7e4e7cca82ea360141030f7623daf88a4d51a0
marinagarces <31573683+marinagarces@users.noreply.github.com>
2021-09-27 01:23:18 -0300

    Avance

--- assets/js-es6/main.js
+++ assets/js-es6/main.js
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import Cliente from './cliente.js';
 import Impuesto from './impuesto.js';
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 "use strict";
 
+require("babel-polyfill");
+
 var _cliente = _interopRequireDefault(require("./cliente.js"));
 
 var _impuesto = _interopRequireDefault(require("./impuesto.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var impuesto1 = new _impuesto["default"](500000, 150);
+var impuesto1 = new _impuesto["default"](100000, 150);
 var impuesto2 = new _impuesto["default"](200000, 250);
 var cliente1 = new _cliente["default"]("ClienteUno", impuesto1);
 var cliente2 = new _cliente["default"]("ClienteUno", impuesto2);
