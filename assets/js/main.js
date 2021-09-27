eef61b8b8139ed8e5229bc59f1081df6a3d30c2c
marinagarces <31573683+marinagarces@users.noreply.github.com>
2021-09-26 23:42:07 -0300

    Avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import Cliente from './cliente.js';
 import Impuesto from './impuesto.js';
 
-let impuesto = new Impuesto(1000, 200);
+let impuesto = new Impuesto(0.19, 150);
 console.log(
   `EL Monto Bruto Anual es: ${impuesto._monto_bruto_anual}, Las Deducciones son: ${impuesto._deducciones}`
 );
